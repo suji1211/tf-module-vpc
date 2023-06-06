e292d77c2cbb2d564c03ec291ba6fccce854eb3c
suji <borra.sujani@gmail.com>
2023-06-06 17:29:24 -0400

    prometheus

--- main.tf
+++ main.tf
@@ -17,3 +17,23 @@ module "subnets" {
   env = var.env
 
 }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, { Name = "${var.env}-igw" })
+}
+
+resource "aws_eip" "ngw" {
+  count = length(var.subnets["public"].cidr_block)
+   vpc   = true
+  tags = merge(var.tags, { Name = "${var.env}-ngw" })
+}
+
+resource "aws_nat_gateway" "ngw" {
+  count = length(var.subnets["public"].cidr_block)
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = module.subnets["public"].subnet_ids[count.index]
+
+  tags = merge(var.tags, { Name = "${var.env}-ngw" })
+
+  }
