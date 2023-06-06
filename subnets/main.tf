2b64c8b97e0dac99d6929c2a46fbb072656ae1ef
suji <borra.sujani@gmail.com>
2023-06-06 17:53:05 -0400

    prometheus

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,5 +1,5 @@
-resource "aws_vpc" "main" {
-  count = length(length(var.cidr_block))
+resource "aws_subnet" "main" {
+  count = length(var.cidr_block)
   vpc_id = var.vpc_id
   cidr_block       = var.cidr_block[count.index]
   availability_zone = var.azs[count.index]
